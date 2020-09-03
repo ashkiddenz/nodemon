ccb4ba48183406cd22f6068040f816897a03f194
Ashley Colaco <ashleycolaco3@gmail.com>
2020-09-04 02:46:11 +0530

    Initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,28 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    'http://api.weatherstack.com/current?access_key=43a22a0cafca13db207277daa4995a42&query=' +
+    latitude +
+    ',' +
+    longitude;
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback('Unable to connect to weather api ', undefined);
+    } else if (body.error) {
+      callback('Unable to find location', undefined);
+    } else {
+      callback(
+        undefined,
+        body.current.weather_descriptions[0] +
+          '.It is currently ' +
+          body.current.temperature +
+          ' degrees out . There is ' +
+          body.current.precip +
+          '% chance of rain.'
+      );
+    }
+  });
+};
+
+module.exports = forecast;
